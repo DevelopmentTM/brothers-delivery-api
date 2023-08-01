2278c2db9ea7231c7c3caf7cbdf57a87075a5d95
Bruno Rafael <brunorafaeldls2016@gmail.com>
2023-07-31 23:01:11 -0300

    feat: starting api project

---
+++ src/main/java/com/developmentteam/brothersdeliveryapi/BrothersDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.developmentteam.brothersdeliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrothersDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrothersDeliveryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developmentteam/brothersdeliveryapi/BrothersDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developmentteam.brothersdeliveryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrothersDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
